d8c86e4d41222505441e3f040a1b0ddac797373a
Galo Sandoval <galosan92@gmail.com>
2023-09-19 19:58:58 -0600

    adds height

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import("./src/env.mjs")
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -15,8 +15,16 @@ const config = {
    */
   i18n: {
     locales: ["en"],
-    defaultLocale: "en",
-  },
-};
+    defaultLocale: "en"
+  }
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "www.google.com"
+  //     }
+  //   ]
+  // }
+}
 
-export default config;
+export default config
