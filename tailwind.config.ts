83d5f71a0d9a533e30ac81d49e5136c6e1c822d5
Albert <albertdang8@gmail.com>
2023-09-21 20:09:51 -0700

    db established

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,70 +7,97 @@ module.exports = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}"
   ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px"
-      }
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))"
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))"
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))"
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))"
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))"
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))"
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))"
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ede178",
+
+          secondary: "#18aebf",
+
+          accent: "#f79e9f",
+
+          neutral: "#311e33",
+
+          "base-100": "#382f3c",
+
+          info: "#4e8ec6",
+
+          success: "#18bf58",
+
+          warning: "#f1ce5b",
+
+          error: "#ee115b"
         }
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)"
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" }
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 }
-        }
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out"
       }
-    }
+    ]
   },
-  plugins: [require("tailwindcss-animate")]
+
+  // theme: {
+  //   container: {
+  //     center: true,
+  //     padding: "2rem",
+  //     screens: {
+  //       "2xl": "1400px"
+  //     }
+  //   },
+  //   extend: {
+  //     colors: {
+  //       border: "hsl(var(--border))",
+  //       input: "hsl(var(--input))",
+  //       ring: "hsl(var(--ring))",
+  //       background: "hsl(var(--background))",
+  //       foreground: "hsl(var(--foreground))",
+  //       primary: {
+  //         DEFAULT: "hsl(var(--primary))",
+  //         foreground: "hsl(var(--primary-foreground))"
+  //       },
+  //       secondary: {
+  //         DEFAULT: "hsl(var(--secondary))",
+  //         foreground: "hsl(var(--secondary-foreground))"
+  //       },
+  //       destructive: {
+  //         DEFAULT: "hsl(var(--destructive))",
+  //         foreground: "hsl(var(--destructive-foreground))"
+  //       },
+  //       muted: {
+  //         DEFAULT: "hsl(var(--muted))",
+  //         foreground: "hsl(var(--muted-foreground))"
+  //       },
+  //       accent: {
+  //         DEFAULT: "hsl(var(--accent))",
+  //         foreground: "hsl(var(--accent-foreground))"
+  //       },
+  //       popover: {
+  //         DEFAULT: "hsl(var(--popover))",
+  //         foreground: "hsl(var(--popover-foreground))"
+  //       },
+  //       card: {
+  //         DEFAULT: "hsl(var(--card))",
+  //         foreground: "hsl(var(--card-foreground))"
+  //       }
+  //     },
+  //     borderRadius: {
+  //       lg: "var(--radius)",
+  //       md: "calc(var(--radius) - 2px)",
+  //       sm: "calc(var(--radius) - 4px)"
+  //     },
+  //     keyframes: {
+  //       "accordion-down": {
+  //         from: { height: 0 },
+  //         to: { height: "var(--radix-accordion-content-height)" }
+  //       },
+  //       "accordion-up": {
+  //         from: { height: "var(--radix-accordion-content-height)" },
+  //         to: { height: 0 }
+  //       }
+  //     },
+  //     animation: {
+  //       "accordion-down": "accordion-down 0.2s ease-out",
+  //       "accordion-up": "accordion-up 0.2s ease-out"
+  //     }
+  //   }
+  // },
+  // plugins: [require("tailwindcss-animate")]
 }
